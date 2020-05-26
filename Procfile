6f98c39b4a24c85af7986bc7988e82b527c63995
zverevaamaria <zverevaamaria@yandex.ru>
2020-05-27 00:54:20 +0300

    Go on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn efficiency.wsgi --log-file -
--- efficiency/settings.py
+++ efficiency/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +28,7 @@ SECRET_KEY = ')ha&_p&!vou)%n11z^760qchn6f13ov&8i+*9$4zzhqe$gkwwn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
